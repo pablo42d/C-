668be6f74a1b6c34d754fd1f802330f30ab676b6
pablo <108802958+pablo42d@users.noreply.github.com>
2025-10-26 23:04:39 +0100

    Add files via upload

---
+++ Lab02/Person.cs
@@ -0,0 +1,79 @@
+﻿using Lab02;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02
+{
+    internal class Person
+    {
+        private string firstName, lastName;
+        private int age;
+
+        //public Person()
+        //{
+        //}
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+        }
+
+        public void View()
+        {
+            Console.WriteLine($"First Name: {firstName}, Last Name: {lastName}, Age: {age}");
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("First name must be at least 2 characters long and cannot be empty.");
+                }
+
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 2)
+                {
+                    throw new ArgumentException("First name must be at least 2 characters long and cannot be empty.");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return Age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be negative.");
+                }
+
+                age = value;
+            }
+        }
+    }
+
+}
+
---
+++ Lab02/Program.cs
@@ -0,0 +1,34 @@
+﻿using Lab02;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Zad.1");      
+        //Person person = new Person();
+        //person.FirstName = "Paweł";
+        //person.LastName = "Drag";
+        //person.Age = 43;
+        //person.View();
+
+        Person[] people = new Person[3];
+        // wypełnianie tablicy obiektami Person
+
+        people[0] = new Person("Alice", "Smith", 34);
+        people[1] = new Person("Bob", "Johnson", 45);
+        people[2] = new Person("Charlie", "Brown", 28);
+        // wywoływanie metody View() dla każdego obiektu w tablicy
+
+        foreach (Person personValue in people)
+        {   personValue.View(); }
+
+
+        //{
+        //    person.View();
+        //    //Parallel.For(0, people.Length, i =>
+        //    //{
+        //    //    people[i].View();
+        //    //});
+        //}
+    }
+}
