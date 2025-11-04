b3316189933fc066048832b25a055a495dd4d5de
pablo42 <adresprywatny@interia.eu>
2025-11-04 20:58:10 +0100

    Connect to SQL and use Select to display the table Druzyna.

---
+++ TestSQL/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestSQL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)  // button click event handler/obsługa zdarzenia kliknięcia przycisku
+        {
+            string connstring = "Data Source= DESKTOP-COV87SH\\SQLEXPRESS;Initial Catalog=w72448;Integrated Security=True"; // connection string/łańcuch połączenia
+            string query = "SELECT * FROM Druzyna"; // SQL query/ zapytanie SQL
+
+            // Using 'using' statements to ensure proper disposal of resources/Użycie instrukcji 'using' w celu zapewnienia prawidłowego zwolnienia zasobów
+            try
+            {
+                using (var conn = new SqlConnection(connstring))    // create and open connection/utworzenie i otwarcie połączenia
+                {
+                    conn.Open();    // open connection/otwarcie połączenia
+                    MessageBox.Show("Connection Opened Successfully");  // confirm successful connection/potwierdzenie pomyślnego połączenia
+
+                    using (var cmd = new SqlCommand(query, conn))   // create command/utworzenie polecenia
+                    using (var reader = cmd.ExecuteReader())    // execute command and get reader/wykonanie polecenia i uzyskanie czytnika
+                    {
+                        while (reader.Read())   // read each row/czytanie każdego wiersza
+                        {
+                            var output = $"Output: {reader.GetValue(0)}_{reader.GetValue(1)}";  // prepare output/przygotowanie wyniku
+                            MessageBox.Show(output);    // display output/wyświetlanie wyniku
+                        }
+                    } // reader and cmd disposed here/tutaj usuwane są reader i cmd
+                } // connection closed and disposed here/tutaj zamykane jest połączenie i usuwane
+            }
+            catch (Exception ex)    // handle exceptions here/tutaj obsługa wyjątków
+            {
+                MessageBox.Show("Error: " + ex.Message);    // display error message/wyświetlanie komunikatu o błędzie
+            }
+        }
+    }
+}
+                 
---
+++ TestSQL/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("TestSQL")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestSQL")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6cb126a5-3dc9-438a-b0f0-96fb7ad5aa6e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
