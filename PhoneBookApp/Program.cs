1ca77287da298eb6ed51ff98ed5f416615cb80b0
pablo42 <adresprywatny@interia.eu>
2025-12-02 00:55:00 +0100

    test

--- PhoneBookApp/Program.cs
+++ PhoneBookApp/Program.cs
@@ -18,7 +18,7 @@ namespace PhoneBookApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new LoginForm())
+            Application.Run(new LoginForm());
         }
     }
 }
