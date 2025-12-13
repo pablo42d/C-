a6d07dcc7373772d3a5c1336711285ac694ef3b5
pablo42 <adresprywatny@interia.eu>
2025-12-13 15:07:32 +0100

    Index zmiany

--- SystemTelefonicznyGY/Controllers/HomeController.cs
+++ SystemTelefonicznyGY/Controllers/HomeController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data;
+using System.Web.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using SystemTelefonicznyGY.Logika; // folder z klasą BazaDanych
+using SystemTelefonicznyGY.Models;
 
 namespace SystemTelefonicznyGY.Controllers
 {
     public class HomeController : Controller
     {
+        private BazaDanych _baza = new BazaDanych();
+
+        public ActionResult Index(string szukanaFraza)
+
         public ActionResult Index()
         {
-            return View();
+            // Zapytanie SQL łączące pracowników z działami
+            string sql = @"SELECT p.Imie, p.Nazwisko, p.Stanowisko, d.NazwaDzialu 
+                           FROM Pracownicy p 
+                           JOIN Dzialy d ON p.ID_Dzialu = d.ID";
+
+            if (!string.IsNullOrEmpty(szukanaFraza))
+            {
+                sql += " WHERE p.Nazwisko LIKE '%" + szukanaFraza + "%' OR d.NazwaDzialu LIKE '%" + szukanaFraza + "%'";
+            }
+
+            DataTable dt = _baza.PobierzDane(sql);
+
+            // Przekazujemy DataTable bezpośrednio do widoku (proste i skuteczne)
+            return View(dt);
         }
 
         public ActionResult About()
