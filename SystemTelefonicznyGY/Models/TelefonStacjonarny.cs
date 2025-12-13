d3f68cae6e88991684084f06752eb5987a4a39de
pablo42 <adresprywatny@interia.eu>
2025-12-13 14:52:35 +0100

    klasa tel stac

---
+++ SystemTelefonicznyGY/Models/TelefonStacjonarny.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SystemTelefonicznyGY.Models
+{
+    public class TelefonStacjonarny : Urzadzenie
+    {
+        // Pola prywatne
+        private string _macAdres;
+        private string _liniaTyp;
+        // Właściwości publiczne
+        public string MacAdres { get { return _macAdres; } }
+        public string LiniaTyp { get { return _liniaTyp; } }
+        public TelefonStacjonarny(int id, string model, string sn, string status, int idPracownika, string macAdres, string liniaTyp) : base(id, model, sn, status, idPracownika)
+        {
+            this._macAdres = macAdres;
+            this._liniaTyp = liniaTyp;
+        }
+
+    }
+}
