d972c2238b968b015db9a40ae2adc7da486b8adc
pablo42 <adresprywatny@interia.eu>
2025-12-30 13:52:15 +0100

    Optymalizacja kodu dla Pracownik .cs

--- SystemTelefonicznyGY/Logika/BilingService.cs
+++ SystemTelefonicznyGY/Logika/BilingService.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 
-namespace SystemTelefonicznyGY.Models
+namespace SystemTelefonicznyGY.Logika
 {
     public class BilingService
     {
@@ -33,5 +34,28 @@ namespace SystemTelefonicznyGY.Models
         LEFT JOIN Dzialy d ON p.ID_Dzialu = d.ID
         WHERE ({warunekCzasu}) {filtrPracownika}";
         }
-    }
+
+        public static (int m, int r) PobierzDateOstatniegoBilingu(BazaDanych baza, int? idPracownika = null)
+        {
+            string filtr = idPracownika.HasValue ? $" WHERE n.ID_Pracownika = {idPracownika.Value}" : "";
+
+            string sql = $@"
+        SELECT TOP 1 YEAR(DataPolaczenia) as R, MONTH(DataPolaczenia) as M 
+        FROM (
+            SELECT b.DataPolaczenia, n.ID_Pracownika FROM BilingiKomorkowe b
+            JOIN NumeryKomorkowe n ON (CASE WHEN LEN(b.NumerTelefonu) > 9 THEN RIGHT(b.NumerTelefonu, 9) ELSE b.NumerTelefonu END) = n.Numer
+            UNION ALL
+            SELECT b.DataPolaczenia, n.ID_Pracownika FROM BilingiStacjonarne b
+            JOIN NumeryStacjonarne n ON (CASE WHEN b.NumerTelefonu LIKE '4814%' OR b.NumerTelefonu LIKE '4822%' THEN RIGHT(b.NumerTelefonu, 7) ELSE b.NumerTelefonu END) = n.Numer
+        ) AS T {filtr}
+        ORDER BY DataPolaczenia DESC";
+
+            DataTable dt = baza.PobierzDane(sql);
+            if (dt != null && dt.Rows.Count > 0)
+            {
+                return (Convert.ToInt32(dt.Rows[0]["M"]), Convert.ToInt32(dt.Rows[0]["R"]));
+            }
+            return (DateTime.Now.Month, DateTime.Now.Year);
+        }
+    }    
 }
--- SystemTelefonicznyGY/Models/Pracownik.cs
+++ SystemTelefonicznyGY/Models/Pracownik.cs
@@ -8,103 +8,139 @@ namespace SystemTelefonicznyGY.Models
 {
     public class Pracownik
     {
-        // Pola prywatne 
-        private int _id;
-        private string _imie;
-        private string _nazwisko;
-        private string _rola;
-        private string _login;
-        private int _idStanowiska;
-        private int _idDzialu;
+        // Właściwości z prywatnym setterem - bezpieczne i zwięzłe
+        public int Id { get; private set; }
+        public string Imie { get; private set; }
+        public string Nazwisko { get; private set; }
+        public string Rola { get; private set; }
+        public string Login { get; private set; }
+        public int IdStanowiska { get; private set; }
+        public int IdDzialu { get; private set; }
 
+        // Właściwość obliczana (Expression-bodied member)
+        public string PelneNazwisko => $"{Imie} {Nazwisko}";
 
-        // Właściwości publiczne na początek
-
-        //public int Id { get; set; }
-        //public string Imie { get; set; }
-        //public string Nazwisko { get; set; }
-        //public string PelneNazwisko { get; set; }
-        //public string Rola { get; set; }
-        //public string Login { get; set; }
-        //public int IdStanowiska { get; set; }
-        //public int IdDzialu { get; set; }
-
-        public int Id { get { return _id; } }
-        public string Imie { get { return _imie; } }
-        public string Nazwisko { get { return _nazwisko; } }
-        public string PelneNazwisko { get { return $"{_imie} {_nazwisko}"; } }
-        public string Rola { get { return _rola; } }
-        public string Login { get { return _login; } }
-        public int IdStanowiska { get { return _idStanowiska; } }
-        public int IdDzialu { get { return _idDzialu; } }
-
-
-        // --- DODATKOWE WŁAŚCIWOŚCI DLA WIDOKU (GET/SET) ---
-        // Te pola wypełnimy danymi z JOIN w kontrolerze
+        // Pozostałe pola dla widoku pozostają bez zmian
         public string NazwaStanowiska { get; set; }
         public string Dzial { get; set; }
         public string NrStacjonarny { get; set; }
         public string NrKomorkowy { get; set; }
-        
 
-        // Konstruktor (zmieniony na 7 parametrów zgodnie z bazą)
         public Pracownik(int id, string imie, string nazwisko, string rola, int idDzialu, string login, int idStanowiska)
         {
-            _id = id;
-            _imie = imie;
-            _nazwisko = nazwisko;
-            _rola = rola;
-            _idDzialu = idDzialu;
-            _login = login;
-            _idStanowiska = idStanowiska;
+            Id = id;
+            Imie = imie;
+            Nazwisko = nazwisko;
+            Rola = rola;
+            IdDzialu = idDzialu;
+            Login = login;
+            IdStanowiska = idStanowiska;
+        }
+
+        public class PodsumowaniePracownikaModel
+        {
+            public int IdPracownika { get; set; }
+            public List<Urzadzenie> MojeUrzadzenia { get; set; } = new List<Urzadzenie>();
+            public List<System.Data.DataRow> BilingiKomorkowe { get; set; } = new List<System.Data.DataRow>();
+            public List<System.Data.DataRow> BilingiStacjonarne { get; set; } = new List<System.Data.DataRow>();
+            public decimal SumaKomorkowe { get; set; }
+            public decimal SumaStacjonarne { get; set; }
         }
     }
+}
+    
 
 
-    //public class Pracownik
-    //{
+
+    //    // Przed optymalizacją:
+    //    // Pola prywatne 
     //    private int _id;
     //    private string _imie;
     //    private string _nazwisko;
     //    private string _rola;
-    //    private int _idDzialu;
     //    private string _login;
     //    private int _idStanowiska;
+    //    private int _idDzialu;
+
+
+//    // Właściwości publiczne na początek
+
+//    //public int Id { get; set; }
+//    //public string Imie { get; set; }
+//    //public string Nazwisko { get; set; }
+//    //public string PelneNazwisko { get; set; }
+//    //public string Rola { get; set; }
+//    //public string Login { get; set; }
+//    //public int IdStanowiska { get; set; }
+//    //public int IdDzialu { get; set; }
+
+//    public int Id { get { return _id; } }
+//    public string Imie { get { return _imie; } }
+//    public string Nazwisko { get { return _nazwisko; } }
+//    public string PelneNazwisko { get { return $"{_imie} {_nazwisko}"; } }
+//    public string Rola { get { return _rola; } }
+//    public string Login { get { return _login; } }
+//    public int IdStanowiska { get { return _idStanowiska; } }
+//    public int IdDzialu { get { return _idDzialu; } }
+
+
+//    // --- DODATKOWE WŁAŚCIWOŚCI DLA WIDOKU (GET/SET) ---
+//    // Te pola wypełnimy danymi z JOIN w kontrolerze
+//    public string NazwaStanowiska { get; set; }
+//    public string Dzial { get; set; }
+//    public string NrStacjonarny { get; set; }
+//    public string NrKomorkowy { get; set; }
+
+
+//    // Konstruktor (zmieniony na 7 parametrów zgodnie z bazą)
+//    public Pracownik(int id, string imie, string nazwisko, string rola, int idDzialu, string login, int idStanowiska)
+//    {
+//        _id = id;
+//        _imie = imie;
+//        _nazwisko = nazwisko;
+//        _rola = rola;
+//        _idDzialu = idDzialu;
+//        _login = login;
+//        _idStanowiska = idStanowiska;
+//    }
+//}
+
+
+////public class Pracownik
+////{
+////    private int _id;
+////    private string _imie;
+////    private string _nazwisko;
+////    private string _rola;
+////    private int _idDzialu;
+////    private string _login;
+////    private int _idStanowiska;
 
 
 
-    //    // Właściwości publiczne do odczytu i zapisu
-    //    public int Id { get { return _id; } }
-    //    public string Imie { get { return _imie; } }
-    //    public string Nazwisko { get { return _nazwisko; } }
-    //    public string PelneNazwisko { get { return $"{_imie} {_nazwisko}"; } }
-    //    public string Rola { get { return _rola; } }
-    //    public int IdDzialu { get { return _idDzialu; } }      
-    //    public string Login { get { return _login; } }
-    //    public int IdStanowiska { get { return _idStanowiska; } }
-    //    // Pole tekstowe do wyświetlania nazwy w tabeli (opcjonalnie)
-    //    public string NazwaStanowiska { get; set; }
+////    // Właściwości publiczne do odczytu i zapisu
+////    public int Id { get { return _id; } }
+////    public string Imie { get { return _imie; } }
+////    public string Nazwisko { get { return _nazwisko; } }
+////    public string PelneNazwisko { get { return $"{_imie} {_nazwisko}"; } }
+////    public string Rola { get { return _rola; } }
+////    public int IdDzialu { get { return _idDzialu; } }      
+////    public string Login { get { return _login; } }
+////    public int IdStanowiska { get { return _idStanowiska; } }
+////    // Pole tekstowe do wyświetlania nazwy w tabeli (opcjonalnie)
+////    public string NazwaStanowiska { get; set; }
 
 
-    //    public Pracownik(int id, string imie, string nazwisko, string rola, int idDzialu, string login, int idStanowiska)
-    //    {
-    //        _id = id;
-    //        _imie = imie;
-    //        _nazwisko = nazwisko;
-    //        _rola = rola;
-    //        _idDzialu = idDzialu;           
-    //        _login = login;
-    //        _idStanowiska = idStanowiska;            
-    //    }
-   
-    //}
-    public class PodsumowaniePracownikaModel
-    {
-        public int IdPracownika { get; set; }
-        public List<Urzadzenie> MojeUrzadzenia { get; set; } = new List<Urzadzenie>();
-        public List<System.Data.DataRow> BilingiKomorkowe { get; set; } = new List<System.Data.DataRow>();
-        public List<System.Data.DataRow> BilingiStacjonarne { get; set; } = new List<System.Data.DataRow>();
-        public decimal SumaKomorkowe { get; set; }
-        public decimal SumaStacjonarne { get; set; }
-    }
-}
+////    public Pracownik(int id, string imie, string nazwisko, string rola, int idDzialu, string login, int idStanowiska)
+////    {
+////        _id = id;
+////        _imie = imie;
+////        _nazwisko = nazwisko;
+////        _rola = rola;
+////        _idDzialu = idDzialu;           
+////        _login = login;
+////        _idStanowiska = idStanowiska;            
+////    }
+
+////}
+///
